b551a5ea13488fd6621fe794d5ad2ab5676bcf82
Santiago Arevalo Valencia <santiguitar2003@gmail.com>
2023-06-01 09:07:00 -0500

    feat: Signer and VerifierSigner working successfully

--- src/model/Verifier.java
+++ src/model/Verifier.java
@@ -4,20 +4,25 @@ import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.PBEParameterSpec;
 
 import java.io.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.*;
 
 public class Verifier {
 
     public KeyPair keys;
-    private static final int RSA_KEY_SIZE = 2048;
+    private static final int RSA_KEY_SIZE = 1024;
+    private static final String KEY_ALGORITHM = "RSA";
+    private static final String SIGNATURE_ALGORITHM = "MD5withRSA";
+    private static final String ENCRYPTION_ALGORITHM = "PBEWithMD5AndDES";
 
     // First, the RSA algorithm must be used to create a key pair (public and private) to digitally sign the file.
 
     public void generateKeyPair(String[] parameters) throws Exception {
         // Create RSA key
-        KeyPairGenerator generatorRSA = KeyPairGenerator.getInstance("RSA");
+        KeyPairGenerator generatorRSA = KeyPairGenerator.getInstance(KEY_ALGORITHM);
         generatorRSA.initialize(RSA_KEY_SIZE);
-        KeyPair keys = generatorRSA.genKeyPair();
+        keys = generatorRSA.genKeyPair();
 
         // Take the encoded form of the public key for future use.
         byte[] bytesPublic = keys.getPublic().getEncoded();
@@ -30,9 +35,6 @@ public class Verifier {
 
         // Repeat the same for the private key, encrypting it with a password
         String privateKey = parameters[1];
-
-        // The path where the private key is to be stored must be saved for upcoming future uses
-        String privatePath = parameters[2] + "/" + privateKey;
 
         // Take the encoded form of the private key
         byte[] bytesPrivate = keys.getPrivate().getEncoded();
@@ -71,28 +73,112 @@ public class Verifier {
 
         // Generate a key from the password and salt
         PBEKeySpec pbeKeySpec = new PBEKeySpec(password.toCharArray());
-        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBE");
+        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance(ENCRYPTION_ALGORITHM);
         SecretKey secretKey = secretKeyFactory.generateSecret(pbeKeySpec);
         PBEParameterSpec pbeParameterSpec = new PBEParameterSpec(salt, 1000);
-        Cipher cipher = Cipher.getInstance("PBEWithMD5AndDES");
+        Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, secretKey, pbeParameterSpec);
 
         // Encrypt the private key
         byte[] encryptedPrivateKeyBytes = cipher.doFinal(privateKeyBytes);
 
-        // Get the parameters of the encryption algorithm
-        AlgorithmParameters algorithmParameters = cipher.getParameters();
-
-        // Save the salt and the encrypted private key to a EncryptedPrivateKeyInfo object
-        EncryptedPrivateKeyInfo encryptedPrivateKeyInfo = new EncryptedPrivateKeyInfo(algorithmParameters, encryptedPrivateKeyBytes);
-
-        return encryptedPrivateKeyInfo.getEncoded();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        baos.write(salt);
+        baos.write(encryptedPrivateKeyBytes);
+        return baos.toByteArray();
     }
 
-    public void signFile() {
+    //Decrypt the private key
+    private byte[] decryptPrivateKey(String password, byte[] encryptedPrivateKeyBytes) throws Exception {
+        // Recover the salt and the encrypted private key
+        byte[] salt = Arrays.copyOfRange(encryptedPrivateKeyBytes, 0, 8);
+        byte[] encryptedPrivateKey = Arrays.copyOfRange(encryptedPrivateKeyBytes, 8, encryptedPrivateKeyBytes.length);
 
+        // Generate the key from the password and salt
+        PBEKeySpec pbeKeySpec = new PBEKeySpec(password.toCharArray());
+        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance(ENCRYPTION_ALGORITHM);
+        SecretKey secretKey = secretKeyFactory.generateSecret(pbeKeySpec);
+        PBEParameterSpec pbeParameterSpec = new PBEParameterSpec(salt, 1000);
+        Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, pbeParameterSpec);
+
+        // Decrypt the private key
+        return cipher.doFinal(encryptedPrivateKey);
     }
 
-    public void verifySignature() {
+    public void signFile(String[] parameters) throws IOException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        String privateKeyPath = parameters[0];
+        String password = parameters[1];
+        String originalFilePath = parameters[2];
+        String signatureFilePath = parameters[3];
+
+        try {
+            InputStream fis = new BufferedInputStream(new FileInputStream(originalFilePath));
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            File file = new File(privateKeyPath);
+            FileInputStream keyFis = new FileInputStream(file);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int i = 0;
+            while ((i = keyFis.read()) != -1) {
+                baos.write(i);
+            }
+            byte[] keyBytes = baos.toByteArray();
+            keyBytes = decryptPrivateKey(password, keyBytes);
+
+            Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+            signature.initSign(keys.getPrivate());
+            signature.update(buffer);
+
+            byte[] digitalSignature = signature.sign();
+            FileOutputStream fileOutputStream = new FileOutputStream(signatureFilePath);
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+            bufferedOutputStream.write(digitalSignature);
+            bufferedOutputStream.close();
+
+            System.out.println("Firma digital generada y guardada en " + signatureFilePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void verifySignature(String[] parameters) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException, IOException, InvalidKeySpecException {
+        String originalFile = parameters[0];
+        String signatureFile = parameters[1];
+        String pbk = parameters[2];
+
+        PublicKey pk = convertToPbKey(pbk);
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+        signature.initVerify(pk);
+        FileInputStream fis = new FileInputStream(signatureFile);
+        byte[] sigFileBytes = new byte[fis.available()];
+        fis.read(sigFileBytes);
+        fis.close();
+        FileInputStream fisTwo = new FileInputStream(originalFile);
+        BufferedInputStream bis = new BufferedInputStream(fisTwo);
+        byte[] buffer = new byte[1024];
+        int len;
+        while (bis.available() != 0) {
+            len = bis.read(buffer);
+            signature.update(buffer, 0, len);
+        }
+        bis.close();
+        boolean isVerified = signature.verify(sigFileBytes);
+        System.out.println("La firma es " + (isVerified ? "válida" : "inválida"));
+    }
+
+    private PublicKey convertToPbKey(String file) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] output = null;
+        try (FileInputStream fis = new FileInputStream(file)) {
+            output = fis.readAllBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        X509EncodedKeySpec keySpec = null;
+        if (output != null) {
+            keySpec = new X509EncodedKeySpec(output);
+        }
+        return keyFactory.generatePublic(keySpec);
     }
 }
--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -12,11 +12,13 @@ public class Menu {
     }
 
     public void showMenu() {
-        System.out.println("Bienvenido al Firmador y Verificafor de Firmas, por favor elija una opción");
+        System.out.println("================================================================================");
+        System.out.println("Bienvenido al Firmador y Verificador de Firmas, por favor elija una opción");
         System.out.println("Seleccione (1) para generar el par de claves RSA");
         System.out.println("Seleccione (2) para firmar un archivo ");
         System.out.println("Seleccione (3) si desea verificar la firma de un archivo");
         System.out.println("Seleccione (4) para salir");
+        System.out.println("================================================================================");
     }
 
     private String[] inputGenerateKeyPair() {
@@ -34,12 +36,37 @@ public class Menu {
         return input;
     }
 
+    private String[] inputSignFile() {
+        String[] input = new String[4];
+        System.out.print("Ingrese la ruta del archivo que contiene la clave privada: ");
+        input[0] = sc.nextLine();
+        System.out.println("Ingrese la contraseña para desencriptar la clave privada: ");
+        input[1] = sc.nextLine();
+        System.out.print("Ingrese la ruta del archivo que desea firmar, con el nombre del archivo incluido: ");
+        input[2] = sc.nextLine();
+        System.out.print("Ingrese la ruta del directorio en el que desea guardar la firma, con el nombre del archivo incluido: ");
+        input[3] = sc.nextLine();
+        return input;
+    }
+
+    private String[] inputVerifySignature() {
+        String[] input = new String[3];
+        System.out.print("Ingrese la ruta del archivo original que desea verificar: ");
+        input[0] = sc.nextLine();
+        System.out.print("Ingrese la ruta del archivo que contiene la firma: ");
+        input[1] = sc.nextLine();
+        System.out.print("Ingrese la ruta del archivo que contiene la llave pública: ");
+        input[2] = sc.nextLine();
+        return input;
+    }
+
     public void doOperation(int option) throws Exception {
         switch (option) {
             case 1 -> verifier.generateKeyPair(inputGenerateKeyPair());
-            case 2 -> verifier.signFile();
-            case 3 -> verifier.verifySignature();
+            case 2 -> verifier.signFile(inputSignFile());
+            case 3 -> verifier.verifySignature(inputVerifySignature());
             case 4 -> System.out.println("Gracias por usar el Firmador y Verificador de Firmas, ¡adiós!");
+            case 5 -> thanks();
             default -> System.out.println("Error, opción no válida");
         }
     }
@@ -52,10 +79,34 @@ public class Menu {
 
     public void startProgram() throws Exception {
         int option;
+        showLogo();
         do {
             showMenu();
             option = readOption();
             doOperation(option);
         } while (option != 4);
+    }
+
+    private void showLogo() {
+        System.out.println(" _____ _ _               _____                      _ _         ");
+        System.out.println("/  __ (_) |             /  ___|                    (_) |        ");
+        System.out.println("| /  \\/_| |__   ___ _ __\\ `--.  ___  ___ _   _ _ __ _| |_ _   _ ");
+        System.out.println("| |   | | '_ \\ / _ \\ '__|`--. \\/ _ \\/ __| | | | '__| | __| | | |");
+        System.out.println("| \\__/\\ | |_) |  __/ |  /\\__/ /  __/ (__| |_| | |  | | |_| |_| |");
+        System.out.println(" \\____/_|_.__/ \\___|_|  \\____/ \\___|\\___|\\__,_|_|  |_|\\__|\\__, |");
+        System.out.println("                                                           __/ |");
+        System.out.println("                                                          |___/ ");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("--------------------------------------------------------------------------------");
+    }
+
+    private void thanks() {
+        System.out.println(" _____                _                ______           __     ");
+        System.out.println("|  __ \\              (_)              | ___ \\       |  / _|    ");
+        System.out.println("| |  \\/_ __ __ _  ___ _  __ _ ___     | |_/ / __ ___ | |_ ___ ");
+        System.out.println("| | __| '__/ _` |/ __| |/ _` / __|     |  __/ '__/ _ \\|  _/ _ \\");
+        System.out.println("| |_\\ \\ | | (_| | (__| | (_| \\__ \\_ _ _| |  | | | (_) | ||  __/");
+        System.out.println(" \\____/_|  \\__,_|\\___|_|\\__,_|___(_|_|_)_|  |_|  \\___/|_| \\___|");
     }
 }
